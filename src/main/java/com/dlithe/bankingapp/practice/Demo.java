0a1b1a982f3b873241140c26384915024341fea8
sanjayjagadishveer <sanjayjagadish8@gmail.com>
2022-04-25 17:46:06 +0530

    "Added  demo class and a method with comments"

---
+++ src/main/java/com/dlithe/bankingapp/practice/Demo.java
@@ -0,0 +1,23 @@
+/**
+ *copyright of my applcation
+ */
+
+
+package com.dlithe.bankingapp.practice;
+
+
+/**
+ * This class is created to push and commit
+ * @author sanjay
+ * @version 1.0
+ */
+public class Demo {
+
+    public void addNumbers(int a,int b)
+    {
+        a=10;
+        b=20;
+        System.out.println(a+b);
+    }
+
+}
